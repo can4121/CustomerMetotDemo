aee25e1f10873ffc577e661cc40749f48f653233
can4121 <89460946+can4121@users.noreply.github.com>
2022-07-01 17:14:57 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Added : " + customer.CustomerName);
+        }
+        public void Deleted(Customer customer)
+        {
+            Console.WriteLine("Deleted : " + customer.CustomerName);
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine("List : " + customer.CustomerName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+using ClassMetotDemo;
+
+Console.WriteLine("Hello, World!");
+
+//3 adet Customer classımdan örnek oluşturdum.
+Customer customer = new Customer();
+customer.Id = 01;
+customer.CustomerName = "Ali";
+customer.CustomerSurname = "Kaya";
+customer.CustomerCity = "İstanbul";
+customer.CustomerAge = 21;
+
+Customer customer1 = new Customer();
+customer1.Id = 02;
+customer1.CustomerName = "Ali";
+customer1.CustomerSurname = "Kaya";
+customer1.CustomerCity = "Ankara";
+customer1.CustomerAge = 16;
+
+
+Customer customer2 = new Customer();
+customer1.Id = 03;
+customer1.CustomerName = "Salih";
+customer1.CustomerSurname = "Kaya";
+customer1.CustomerCity = "İzmir";
+customer1.CustomerAge = 25;
+
+
+//Örneklediğim Customer'ları Customer tipi içerisinde diziye ekledim
+Customer[] customers = new Customer[] { customer, customer1, customer2 };
+//diziyi foreach ile dönerek çıktısını ekrana yazdırdım.
+foreach(var item in customers)
+{
+    Console.WriteLine("Müşteri adı : " + item.CustomerName);
+}
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(customer);
+customerManager.List(customer1);
+customerManager.Deleted(customer2);
